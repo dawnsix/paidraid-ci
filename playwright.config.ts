32d6edc5f652fad11062f2e1a3559c5ea1357bd0
dawnsix <d4wnfr33z3@gmail.com>
2023-02-26 17:09:03 +1000

    restore cfg to default

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,19 +25,12 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 1 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter: '@butchmayhew/playwright-json-summary-reporter',
-  //reporter: 'dot',
-  //reporter: [['html', { open: 'never' }]],
-  reporter: [ 
-  //  ['@butchmayhew/playwright-json-summary-reporter'],
-    ['html', { open: 'never' }] 
-  ],
-  
+  //reporter: 'html',
+  reporter: [ ['html', { open: 'never' }] ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -47,7 +40,6 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    //trace: 'off',
   },
 
   /* Configure projects for major browsers */
