818e911f37816684ec61f6269eb8f95f965c16f1
dawnsix <d4wnfr33z3@gmail.com>
2023-02-26 15:49:46 +1000

    disable trace

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: '@butchmayhew/playwright-json-summary-reporter',
   //reporter: 'html',
@@ -46,7 +46,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    //trace: 'on-first-retry',
+    trace: 'off',
   },
 
   /* Configure projects for major browsers */
