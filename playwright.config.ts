867ca150ebe0f8ee086b3f09608137da1bcb619d
dawnsix <d4wnfr33z3@gmail.com>
2023-02-26 14:24:35 +1000

    get rid of json reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'html',
   reporter: [ 
-    ['@butchmayhew/playwright-json-summary-reporter'],
+    //['@butchmayhew/playwright-json-summary-reporter'],
     ['html', { open: 'never' }] 
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
