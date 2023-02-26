396e398d76f5bc2b05e7be49b4e8ecbb8cee96f9
dawnsix <d4wnfr33z3@gmail.com>
2023-02-26 17:05:40 +1000

    restore trace

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,8 +46,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    //trace: 'on-first-retry',
-    trace: 'off',
+    trace: 'on-first-retry',
+    //trace: 'off',
   },
 
   /* Configure projects for major browsers */
