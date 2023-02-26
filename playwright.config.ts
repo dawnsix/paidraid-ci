8d7e145299099517494fa16dc0911b8ba22ebfb0
dawnsix <d4wnfr33z3@gmail.com>
2023-02-26 17:02:32 +1000

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,12 +31,12 @@ export default defineConfig({
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: '@butchmayhew/playwright-json-summary-reporter',
-  reporter: 'dot',
+  //reporter: 'dot',
   //reporter: [['html', { open: 'never' }]],
-  /*reporter: [ 
+  reporter: [ 
   //  ['@butchmayhew/playwright-json-summary-reporter'],
     ['html', { open: 'never' }] 
-  ],*/
+  ],
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
