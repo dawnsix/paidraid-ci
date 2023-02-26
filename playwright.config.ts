0cef73890eb696879869aad57997d3449adec295
dawnsix <d4wnfr33z3@gmail.com>
2023-02-26 14:52:33 +1000

    update reporter for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,12 +29,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: '@butchmayhew/playwright-json-summary-reporter',
   //reporter: 'html',
-  reporter: 'line',
+  //reporter: 'line',
   /*reporter: [ 
     ['@butchmayhew/playwright-json-summary-reporter'],
     ['html', { open: 'never' }] 
   ],*/
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
